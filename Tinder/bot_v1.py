df714aa0db19357ca9070312a7fac5c3da5bd533
Haydn-Martin <haydnmartin6@gmail.com>
2022-11-30 13:12:38 +0000

    Adding mouse click and new sessions

--- Tinder/bot_v1.py
+++ Tinder/bot_v1.py
@@ -1,5 +1,6 @@
 import random
 import time
+import pyautogui as pyaut
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -19,45 +20,61 @@ driver = webdriver.Chrome(service=service, options=options)
 # web address
 web = 'https://tinder.com/'
 
-driver.get(web) #open tinder in chrome
-time.sleep(5)
+#driver.get(web) #open tinder in chrome
+#time.sleep(5)
 
 ### Swiping ###
 
-# Find way to move mouse as well to keep screen active!
+# TODO
 # Exit the match screen to prevent failures when pop up
 
 #no_swipes = 20 #for testing
 like_ratio_rand = random.randint(10, 20)
-swipes = 200
+swipe_sesh = 20
 
-for i in range(swipes):
-    try:
-        score = random.randint(0, 100) #attractiveness - rand for now
-        if score > like_ratio_rand:
-            # liking someone
-            like_xpath = '//button//span[text()="Like"]' #like button element
-            like_location = driver.find_element(by='xpath', value=like_xpath) #find the like button
-            driver.execute_script("arguments[0].click();", like_location) #click on like
-        else:
-            nope_xpath = '//button//span[text()="Nope"]'  # like button element
-            nope_location = driver.find_element(by='xpath', value=nope_xpath)  # find the like button
-            driver.execute_script("arguments[0].click();", nope_location)  # click on like
-
-        # pause between swipes
-        sleep_time = random.randint(2, 4)
-        time.sleep(sleep_time)  # pause from a random amount of seconds to stop bot protection
-
-        # closing match pop up
-        close_match_xpath = '//button[@title=Back to Tinder"]'
-        close_match_window = driver.find_element(by='xpath', value =close_match_xpath)
-        close_match_window.click()
-
-    except:
+while True:
+    i = 0
+    driver.get(web)  # open tinder in chrome
+    time.sleep(5)
+    for i in range(swipe_sesh):
         try:
-            # closing pop ups
-            pop_up_close_txt_xpath = '//button/span[text()="Maybe Later"] | //button/span[text()="Not interested"] | //button/span[text()="No Thanks"]'
-            close_pop_up = driver.find_element(by='xpath', value=pop_up_close_txt_xpath)
-            close_pop_up.click()
+            score = random.randint(0, 100) #attractiveness - rand for now
+            if score > like_ratio_rand:
+                # liking someone
+                like_xpath = '//button//span[text()="Like"]' #like button element
+                like_location = driver.find_element(by='xpath', value=like_xpath) #find the like button
+                driver.execute_script("arguments[0].click();", like_location) #click on like
+                time.sleep(1)
+            else:
+                # noping someone
+                nope_xpath = '//button//span[text()="Nope"]'  # like button element
+                nope_location = driver.find_element(by='xpath', value=nope_xpath)  # find the like button
+                driver.execute_script("arguments[0].click();", nope_location)  # click on like
+                time.sleep(1)
+
+            # moving mouse and clicking
+            if i % 2 == 0:
+                pyaut.moveRel(0, 30)
+            else:
+                pyaut.moveRel(0, -30)
+            pyaut.click()
+            i+=1
+
+            # closing match pop up
+            close_match_xpath = '//button[@title=Back to Tinder"]'
+            close_match_window = driver.find_element(by='xpath', value =close_match_xpath)
+            close_match_window.click()
+            time.sleep(1)
+
+            # pause between swipes
+            sleep_time = random.randint(2, 4)
+            time.sleep(sleep_time)  # pause from a random amount of seconds to stop bot protection
+
         except:
-            pass
+            try:
+                # closing pop ups
+                pop_up_close_txt_xpath = '//button/span[text()="Maybe Later"] | //button/span[text()="Not interested"] | //button/span[text()="No Thanks"]'
+                close_pop_up = driver.find_element(by='xpath', value=pop_up_close_txt_xpath)
+                close_pop_up.click()
+            except:
+                pass
